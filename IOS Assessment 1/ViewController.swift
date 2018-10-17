9f67b1de53af2bab9800725d6205df3677b54029
Sawyer Shirley <sawyershirley@MacBook-Pro-107.local>
2018-10-17 13:22:35 -0500

    Finished Assessment 1

--- IOS Assessment 1/ViewController.swift
+++ IOS Assessment 1/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    var textInput = ""
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        textInput = textField.text!
+        label.text = textInput
+        textField.text = ""
+    }
+    
 }
 
