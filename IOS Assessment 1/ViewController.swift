b9ee4cca2c6a3d6dffb08e57d39ccddef92f7cc2
Sawyer Shirley <sawyershirley@MacBook-Pro-107.local>
2018-10-17 12:20:35 -0500

    Initial Commit

---
+++ IOS Assessment 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS Assessment 1
+//
+//  Created by Sawyer Shirley on 10/17/18.
+//  Copyright © 2018 Sawyer Shirley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
